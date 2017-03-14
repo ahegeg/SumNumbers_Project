ce43ec3283451a1cebac61d79bbc91a32386c70a
ahegeg <ahegeg@gmail.com>
2017-03-14 05:45:29 +0200

    Add the whole solution

---
+++ SumBumbers_BLL/SumBumbers_BLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumBumbers_BLL
+{
+    public class SumBumbers_BLL
+    {
+        public double SumNumbers(double x, double y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ SumNumbers_Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumNumbers_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter X");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y");
+            double y = double.Parse( Console.ReadLine());
+
+            SumBumbers_BLL.SumBumbers_BLL bll = new SumBumbers_BLL.SumBumbers_BLL();
+
+            Console.WriteLine("Results:" + bll.SumNumbers(x, y).ToString());
+        }
+    }
+}
