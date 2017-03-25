9bda003eb8d14b0eef78e498a589564cbf246c36
ahegeg <ahegeg@gmail.com>
2017-03-25 21:35:36 +0200

    fix the test

--- SumNumbers.Test/SumBumbers_Test.cs
+++ SumNumbers.Test/SumBumbers_Test.cs
@@ -16,7 +16,6 @@ namespace SumNumbers.Test
         [TestMethod]
         public void TestMethod2()
         {
-
             SumBumbers_BLL.SumBumbers_BLL testObj = new SumBumbers_BLL.SumBumbers_BLL();
             Assert.AreEqual(testObj.SumNumbers(10, 4), 14,"Sum numbers fail");
            
