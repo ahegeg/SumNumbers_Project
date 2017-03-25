a12cb7f65a50eb34ad919b2122399e1de7e3a6f7
ahegeg <ahegeg@gmail.com>
2017-03-25 20:33:49 +0200

    Adding a test class

---
+++ SumNumbers.Test/SumBumbers_Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SumNumbers.Test
+{
+    [TestClass]
+    public class SumBumbers_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SumBumbers_BLL.SumBumbers_BLL testObj = new SumBumbers_BLL.SumBumbers_BLL();
+            Assert.AreEqual(testObj.SumNumbers(10, 4), 14);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SumBumbers_BLL.SumBumbers_BLL testObj = new SumBumbers_BLL.SumBumbers_BLL();
+            Assert.AreEqual(testObj.SumNumbers(10, 4), 15,"Sum numbers fail");
+           
+        }
+    }
+}
