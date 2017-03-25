f63862f900cb5400813d5a4129861f28c02bc1a4
ahegeg <ahegeg@gmail.com>
2017-03-25 21:29:29 +0200

    CommitTest

--- SumNumbers.Test/SumBumbers_Test.cs
+++ SumNumbers.Test/SumBumbers_Test.cs
@@ -16,6 +16,7 @@ namespace SumNumbers.Test
         [TestMethod]
         public void TestMethod2()
         {
+
             SumBumbers_BLL.SumBumbers_BLL testObj = new SumBumbers_BLL.SumBumbers_BLL();
             Assert.AreEqual(testObj.SumNumbers(10, 4), 14,"Sum numbers fail");
            
